3a5f0b6f69c8a1e9237b6cd8060aae75867aaeb8
umahato <47037548+umahato@users.noreply.github.com>
2021-03-12 20:46:53 +0530

    Create main.tf

---
+++ import/main.tf
@@ -0,0 +1,12 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+resource "aws_vpc" "myvpc2" {
+  cidr_block = "192.168.0.0/24"
+}
