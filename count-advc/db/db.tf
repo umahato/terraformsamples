a6ece50fb066163642dc0d1f9cfa7230f71c1568
umahato <47037548+umahato@users.noreply.github.com>
2021-03-12 20:40:03 +0530

    Create db.tf

---
+++ count-advc/db/db.tf
@@ -0,0 +1,18 @@
+variable "server_names" {
+  type = list(string)
+}
+
+resource "aws_instance" "db" {
+  ami="ami-0eeb03e72075b9bcc"
+  instance_type ="t2.micro"
+  count = length(var.server_names)
+
+  tags = {
+      Name = var.server_names[count.index]
+  }
+} 
+
+
+output "PrivateIP" {
+    value=[aws_instance.db.*.private_ip]
+  }
